c5a68c65ae6e791e319e1895328e154ebc15dae3
Efrain <efrainc.dev@gmail.com>
2025-08-09 22:08:35 +0300

    type fix

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -17,10 +17,10 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    },
-    authMethodsPerActor: {
-      user: ["session", "bearer", "emailpass"],
-      customer: ["session", "bearer", "emailpass"]
+      authMethodsPerActor: {
+        user: ["session", "bearer", "emailpass"],
+        customer: ["session", "bearer", "emailpass"]
+      }
     }
   },
   admin: {
