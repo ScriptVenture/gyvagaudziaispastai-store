50726890dd81bf2b0e659e0af075aa422af32bd5
Efrain <efrainc.dev@gmail.com>
2025-08-10 09:20:28 +0300

    fix medusa.ts to load images properly in admin panel

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -37,7 +37,7 @@ module.exports = defineConfig({
             id: "local",
             options: {
               upload_dir: "static",
-              backend_url: process.env.MEDUSA_BACKEND_URL || "http://localhost:9000",
+              backend_url: process.env.MEDUSA_BACKEND_URL,
             },
           },
         ],
